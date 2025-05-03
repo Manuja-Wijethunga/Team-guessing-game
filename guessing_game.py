1b4517626cc2af7c1cdf7249f9b438f31334944f
DinuIDH <imashidinu2003@gmail.com>
2025-05-03 07:09:10 +0530

    added hints

--- guessing_game.py
+++ guessing_game.py
@@ -9,3 +9,12 @@ if guess == number:
     print("You win!")
 else:
     print(f"Wrong! The number was {number}")
+    
+    guess = int(input("Take a guess: "))
+
+    if guess < number:
+        print("Too low!")
+    elif guess > number:
+        print(" Too high!")
+    else:
+        print(" You win!")
